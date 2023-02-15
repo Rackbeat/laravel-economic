fb233bee70d1016492dcee24e3fa90bf72b111ff
Andrej Biljanoski <anb@rackbeat.com>
2023-02-15 13:28:24 +0100

    Add valid method in ProductCurrencyPriceBuilder

--- src/Builders/ProductCurrencyPriceBuilder.php
+++ src/Builders/ProductCurrencyPriceBuilder.php
@@ -3,10 +3,12 @@
 namespace LasseRafn\Economic\Builders;
 
 use LasseRafn\Economic\Models\ProductCurrencyPrice;
+use LasseRafn\Economic\Utils\Model;
 use LasseRafn\Economic\Utils\Request;
 
 class ProductCurrencyPriceBuilder extends Builder
 {
+    public const VALID_CURRENCIES = ['DKK', 'EUR', 'GBP', 'NOK', 'SEK', 'USD'];
     protected $entity = 'products/:productNumber/pricing/currency-specific-sales-prices';
     protected $model = ProductCurrencyPrice::class;
 
@@ -15,5 +17,12 @@ class ProductCurrencyPriceBuilder extends Builder
         $this->entity = str_replace(':productNumber', $productNumber, $this->entity);
 
         parent::__construct($request);
+    }
+
+    public function valid(ProductCurrencyPriceBuilder $builder)
+    {
+        return $builder->all([])->filter(function ($currency) {
+            return in_array($currency->currency->code, self::VALID_CURRENCIES);
+        })->values();
     }
 }
