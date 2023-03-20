a4ee637664c73058969d72d7a977818d35286985
Andrej Biljanoski <anb@rackbeat.com>
2023-03-20 18:40:07 +0100

    Add departments builder

---
+++ src/Models/Department.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LasseRafn\Economic\Models;
+
+use LasseRafn\Economic\Utils\Model;
+
+class Department extends Model
+{
+    protected $entity = 'department';
+    protected $primaryKey = 'number';
+
+    public $departmentNumber;
+    public $name;
+}
