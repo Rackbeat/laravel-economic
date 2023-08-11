b9b462baa194e80d9c0fc66ca9e58eb6376b8e72
Andrej Biljanoski <anb@rackbeat.com>
2023-08-11 14:57:21 +0200

    Add properties in AccountPeriod Model

--- src/Models/AccountingPeriod.php
+++ src/Models/AccountingPeriod.php
@@ -2,11 +2,15 @@
 
 namespace LasseRafn\Economic\Models;
 
-use LasseRafn\Economic\Builders\AccountingPeriodTotalsBuilder;
 use LasseRafn\Economic\Utils\Model;
 
 class AccountingPeriod extends Model
 {
 	protected $entity = ' accounts/:account/accounting-years/:accountingYear/periods';
 	protected $primaryKey = 'periodNumber';
+
+	public $periodNumber;
+	public $accountingYear;
+	public $fromDate;
+	public $toDate;
 }
