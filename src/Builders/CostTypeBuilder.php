df92da67c0d36fc64b62b04892f62981c6175cc8
Andrej Biljanoski <anb@rackbeat.com>
2024-01-03 10:35:28 +0100

    Extend from new rest builder

--- src/Builders/CostTypeBuilder.php
+++ src/Builders/CostTypeBuilder.php
@@ -4,7 +4,7 @@ namespace LasseRafn\Economic\Builders;
 
 use LasseRafn\Economic\Models\CostType;
 
-class CostTypeBuilder extends Builder
+class CostTypeBuilder extends newRestBuilder
 {
 	protected $entity = 'costtypes';
 	protected $model = CostType::class;
