718d4409187697e8ba84b6c0bea5605d9589bc16
Aleksandar <mitevskialeksandar@outlook.com>
2024-10-07 11:05:34 +0200

    updated builder class

--- src/Builders/v1/SupplierGroup/SupplierGroupBuilder.php
+++ src/Builders/v1/SupplierGroup/SupplierGroupBuilder.php
@@ -5,9 +5,24 @@ namespace LasseRafn\Economic\Builders\v1\SupplierGroup;
 use LasseRafn\Economic\Builders\v1\RestBuilderV1;
 use LasseRafn\Economic\Models\SupplierGroup;
 
-class SupplierGroupBuilder extends RestBuilderV1
+class SupplierGroupBuilder extends BaseBuilder
 {
+	const VERSION = 'v1.0.1';
+
 	protected $rest_api = 'suppliersapi';
 	protected $entity = 'Groups';
 	protected $model  = SupplierGroup::class;
+
+	protected $rest_version = '/';
+
+	public function __construct(Request $request)
+	{
+		parent::__construct($request);
+		$this->rest_version .= $this->rest_api . '/' . self::VERSION;
+	}
+
+	/** Overrides BaseBuilder */
+	protected function getItemsFromResponse($response){
+		return json_decode($response->getBody()->getContents())->items;
+	}
 }
