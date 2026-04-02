643540483fb07b9d6481b921bc4170e082366010
Aleksandar <mitevskialeksandar@outlook.com>
2026-04-02 09:36:15 +0200

    expand SupplierGroup model to include the full path to enable updates

--- src/Models/SupplierGroup.php
+++ src/Models/SupplierGroup.php
@@ -8,4 +8,10 @@ class SupplierGroup extends Model
 {
 	protected $entity     = 'Groups';
 	protected $primaryKey = 'number';
+	protected $rest_version = 'suppliersapi/v2.0.0';
+
+	public $accountNumber;
+	public $name;
+	public $number;
+	public $objectVersion;
 }
